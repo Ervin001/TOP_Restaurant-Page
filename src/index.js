edb964d98ff98afff67723cdefcaa2d912bc0b29
Ervin <evgnz24@gmail.com>
2022-08-27 08:14:16 -0500

    fixed index.js file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
 import _ from 'lodash';
-HEAD
-import printMe from './print.js';
-
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-document.body.appendChild(component());
 
 import './style.css';
 import Icon from './icon.png';
@@ -25,4 +7,3 @@ import home from './pages/home.js';
 (function component() {
   home();
 })();
-master
