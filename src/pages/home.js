fa8e5c3067b25de37100db4e3cf47d6f7d19517b
Ervin <evgnz24@gmail.com>
2022-08-26 22:57:28 -0500

    added some colors to the nav and changed the font-color as well.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,33 @@
 const home = function () {
-  const containerEl = document.createElement('div');
+  const containerEl = document.createElement('section');
   document.body.appendChild(containerEl);
 
   const html = `
     <div class="container">
-      <div class="logo-container">
+      <nav class="logo-container">
         <div class="title">Ozone</div>
         <div class="tabs-container">
           <div class="tab home-tab">Home</div>
           <div class="tab menu-tab">Menu</div>
           <div class="tab aboutUs-tab">About Us</div>
         </div>
+      </nav>
+      <div class="main-content-container">
+        <div class="welcome-text">Welcome to Ozone</div>
+        <div class="short-desc">    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto saepe modi omnis nulla quisquam quidem aliquid error voluptatibus ipsa in porro vitae culpa obcaecati reprehenderit fuga magnam, veniam consectetur doloremque.</div>
+        <div class="image"></div>
       </div>
+      <footer>
+        <div class="footer-info">
+          <div class="created-by">Created by <strong>Ervin001</strong></div>
+          <div class="extra-info"></div>
+        </div>
+      </footer>
     </di>
   `;
 
-  containerEl.insertAdjacentHTML('afterend', html);
+  containerEl.insertAdjacentHTML('beforeend', html);
+  containerEl;
 };
 
 export default home;
